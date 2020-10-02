afc1a3c20d9dc831fe290e1ebfeb3635561abd4d
michael-bailey <mickyb18a@gmail.com>
2020-10-02 08:16:54 +0100

    Initial Commit

---
+++ iOS-Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS-Calculator
+//
+//  Created by michael bailey on 02/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Calculator/iOS_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CalculatorApp.swift
+//  iOS-Calculator
+//
+//  Created by michael bailey on 02/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
